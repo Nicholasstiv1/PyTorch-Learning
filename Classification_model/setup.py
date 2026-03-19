53606f9eac79473c4d0e473b3e49d6fe37015d67
Nicholasstiv1 <nicholas.guerino@gmail.com>
2026-03-19 16:19:57 -0400

    Training the model and helper functions

--- Classification_model/setup.py
+++ Classification_model/setup.py
@@ -1,6 +1,10 @@
 import torch
 from CircleModel import CircleModel
+from data import random_split
 from torch import nn
+from utils.accuracyFunction import accuracy_fn
+
+torch.manual_seed(42)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model_0 = CircleModel().to(device)
@@ -9,3 +13,49 @@ loss_fn = nn.BCEWithLogitsLoss()  # Sigmoid activation function built-in
 
 optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.1)
 
+epochs = 100
+
+# Put data to target device
+X_train, X_test, y_train, y_test = random_split()
+X_train, X_test = X_train.to(device), X_test.to(device)
+y_train, y_test = y_train.to(device), y_test.to(device)
+
+# Build training and evaluation loop
+for epoch in range(epochs):
+    model_0.train()
+
+    # 1. Forward pass
+    y_logits = model_0(X_train).squeeze()
+    y_pred = torch.round(
+        torch.sigmoid(y_logits)
+    )  # turn logits -> pred probs -> pred labels
+
+    # 2. Calculate loss/accuracy
+    loss = loss_fn(
+        y_logits, y_train
+    )  # nn.BCEWithLogitsLoss expects raw logits as inputs
+    acc = accuracy_fn(y_true=y_train, y_pred=y_pred)
+
+    # 3. Optimizer zero grad
+    optimizer.zero_grad()
+
+    # 4. Loss backward
+    loss.backward()
+
+    # 5. Optimizer step
+    optimizer.step()
+
+    ### Testing
+    model_0.eval()
+    with torch.inference_mode():
+        test_logits = model_0(X_test).squeeze()
+        test_pred = torch.round(torch.sigmoid(test_logits))
+
+        test_loss = loss_fn(test_logits, y_test)
+        test_acc = accuracy_fn(y_true=y_test, y_pred=test_pred)
+
+    # Print out what's happening
+    if epoch % 10 == 0:
+        print(
+            f"Epoch: {epoch} | Loss: {loss:.5f}, Acc: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test Acc: {test_acc:.2f}%"
+        )
